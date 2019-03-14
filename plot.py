4f6edfd118417c064d7274576539c7b7a86882ef
rijingxi <rjxi@yahoo.com>
2019-03-13 22:20:39 -0400

    First draft of script 01

---
+++ plot.py
@@ -0,0 +1,15 @@
+#This is code to test anacoda package 
+
+# import package
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,20,100)
+
+plt.plot(x,np.sin(x))
+
+plt.show()
+
+
+#some changes 
---
+++ test1.py
@@ -0,0 +1,24 @@
+"""
+This is a tets code to use Viusal Studio Code 
+-- Installing Anaconda Enviornment
+-- One issue if starting VSCode outside Anaconda Navigator, Cannot import package in Conda
+    But if starting VSCode from Anacoda Navigator, Package can be improted
+-- Solution 
+  1. In Conda activate  env first, then run code
+
+"""
+
+msg = 'Hello World'
+
+print(msg)
+
+if msg.isalnum():
+    print('all number')
+else:
+    print('Not all number')
+	
+# adding some changes to test github
+
+#more changes 
+
+# more changes 
