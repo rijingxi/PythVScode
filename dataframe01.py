54a6d5ad3702092e069c2f900829733bd837dc9a
rijingxi <rjxi@yahoo.com>
2019-04-08 17:09:15 -0400

    add dataframe code

--- dataframe01.py
+++ dataframe01.py
@@ -1,6 +1,21 @@
 """
 Pandas dataframe code
 """
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+
+# Creating DataFrame
+# specify values for each column
+
+df = pd.DataFrame(
+    {"a": [4, 5, 6],
+     "b": [7, 8, 9],
+     "c": [10, 11, 12]
+     },
+    index=[1, 2, 3]
+
+)
+
+df.head()
+
+print(df.shape)
 
